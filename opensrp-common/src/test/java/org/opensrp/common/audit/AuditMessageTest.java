d8cf021af13b62c7d3175a3c9db79704489ad7ca
bitaron <bitaron90@gmail.com>
2017-06-19 16:08:32 +0600

    dd

--- opensrp-common/src/test/java/org/opensrp/common/audit/AuditMessageTest.java
+++ opensrp-common/src/test/java/org/opensrp/common/audit/AuditMessageTest.java
@@ -3,6 +3,10 @@ package org.opensrp.common.audit;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.Test;
+import org.opensrp.common.util.ComparableTester;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AuditMessageTest {
 
@@ -12,5 +16,48 @@ public class AuditMessageTest {
                 .usingGetClass()
                 .withOnlyTheseFields("messageIndex")
                 .verify();
+    }
+
+    @Test
+    public void testComparisonReversal() {
+        AuditMessage one = new AuditMessage(null, 123l, null, null);
+        AuditMessage sameAsOne = new AuditMessage(null, 123l, null, null);
+        AuditMessage different = new AuditMessage(null, 321l, null, null);
+
+        ComparableTester.assertComparisonReversal(one, sameAsOne);
+        ComparableTester.assertComparisonReversal(sameAsOne, different);
+        ComparableTester.assertComparisonReversal(one, different);
+    }
+
+    @Test
+    public void testConsistencyWithEqual() {
+        AuditMessage one = new AuditMessage(null, 123l, null, null);
+        AuditMessage sameAsOne = new AuditMessage(null, 123l, null, null);
+
+        ComparableTester.assertConsistencyWithEqual(one, sameAsOne);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullPointerException(){
+        AuditMessage one = new AuditMessage(null, 123l, null, null);
+        ComparableTester.assertNullPointerException(one);
+    }
+
+    @Test
+    public void testTransitivity() {
+        AuditMessage one = new AuditMessage(null, 123l, null, null);
+        AuditMessage two = new AuditMessage(null, 300l, null, null);
+        AuditMessage third = new AuditMessage(null, 321l, null, null);
+
+        ComparableTester.assertTransitivity(one, two, third);
+    }
+
+    @Test
+    public void testConsistency() {
+        AuditMessage one = new AuditMessage(null, 123l, null, null);
+        AuditMessage sameAsOne = new AuditMessage(null, 123l, null, null);
+        AuditMessage different = new AuditMessage(null, 321l, null, null);
+
+        ComparableTester.assertConsistency(one, sameAsOne, different);
     }
 }
