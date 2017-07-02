7d5cfc6d329dae4aba8c02cce1830e10214ce0b6
Proshanto <proshanto@mpower-social.com>
2017-07-02 18:02:51 +0600

    test code on form repository

---
+++ opensrp-form/src/test/java/org/opensrp/form/domain/FormDataTest.java
@@ -0,0 +1,22 @@
+package org.opensrp.form.domain;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FormDataTest {
+	
+	@Before
+    public void setUp() throws Exception {
+        
+    }
+	
+	@Test
+	public void shouldTestToString(){
+		/*EqualsVerifier.forClass(FormData.class)
+		.verify();*/
+		
+	}
+
+}
