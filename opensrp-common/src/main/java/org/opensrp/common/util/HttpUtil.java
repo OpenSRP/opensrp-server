69ea6f39c20b70733a9cc9731813360db35f39cc
bitaron <bitaron90@gmail.com>
2017-07-05 17:51:21 +0600

    Format

--- opensrp-common/src/main/java/org/opensrp/common/util/HttpUtil.java
+++ opensrp-common/src/main/java/org/opensrp/common/util/HttpUtil.java
@@ -25,48 +25,48 @@ import java.security.KeyStore;
 public class HttpUtil {
 
 
-	private HttpUtil() {
+    private HttpUtil() {
 
     }
 
-	public enum AuthType{
-		BASIC, TOKEN, NONE
-	}
-	
+    public enum AuthType {
+        BASIC, TOKEN, NONE
+    }
+
     private final static DefaultHttpClient httpClient = init();
 
-	public static DefaultHttpClient init()  {
-    	try {
-	    	//TODO add option to ignore cetificate validation in opensrp.prop
-	    	KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-			trustStore.load(null, null);
-			CustomCertificateSSLSocketFactory sf = new CustomCertificateSSLSocketFactory(trustStore);
-			sf.setHostnameVerifier(CustomCertificateSSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-			
-	        BasicHttpParams basicHttpParams = new BasicHttpParams();
-	        HttpConnectionParams.setConnectionTimeout(basicHttpParams, 30000);
-	        HttpConnectionParams.setSoTimeout(basicHttpParams, 60000);
-	
-	        SchemeRegistry registry = new SchemeRegistry();
-	        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-	        registry.register(new Scheme("https", sf, 443));
-	
-	        ClientConnectionManager connectionManager = new ThreadSafeClientConnManager(basicHttpParams, registry);
-	        return new DefaultHttpClient(connectionManager, basicHttpParams);
-    	} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
+    public static DefaultHttpClient init() {
+        try {
+            //TODO add option to ignore cetificate validation in opensrp.prop
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+            CustomCertificateSSLSocketFactory sf = new CustomCertificateSSLSocketFactory(trustStore);
+            sf.setHostnameVerifier(CustomCertificateSSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            BasicHttpParams basicHttpParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(basicHttpParams, 30000);
+            HttpConnectionParams.setSoTimeout(basicHttpParams, 60000);
+
+            SchemeRegistry registry = new SchemeRegistry();
+            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+            registry.register(new Scheme("https", sf, 443));
+
+            ClientConnectionManager connectionManager = new ThreadSafeClientConnManager(basicHttpParams, registry);
+            return new DefaultHttpClient(connectionManager, basicHttpParams);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
-    public static HttpResponse post(String url, String payload, String data, String username,String password) {
-        return post(url, payload, data, "application/json", AuthType.BASIC, username+":"+password);
+    public static HttpResponse post(String url, String payload, String data, String username, String password) {
+        return post(url, payload, data, "application/json", AuthType.BASIC, username + ":" + password);
     }
-    
+
     public static HttpResponse post(String url, String payload, String data) {
         return post(url, payload, data, "application/json", AuthType.NONE, "");
     }
-    
+
     public static HttpResponse postWithToken(String url, String payload, String data, String token) {
         return post(url, payload, data, "application/json", AuthType.TOKEN, token);
     }
@@ -74,39 +74,39 @@ public class HttpUtil {
     //TODO: Move setting content type in makeConnection function.
     public static HttpResponse post(String url, String payload, String data, String contentType, AuthType authType, String authString) {
         try {
-        	HttpPost request = (HttpPost) makeConnection(url, payload, RequestMethod.POST, authType, authString);
+            HttpPost request = (HttpPost) makeConnection(url, payload, RequestMethod.POST, authType, authString);
             request.setHeader(HTTP.CONTENT_TYPE, contentType);
-            StringEntity entity = new StringEntity(data==null?"":data);
+            StringEntity entity = new StringEntity(data == null ? "" : data);
             System.out.println(data);
             entity.setContentEncoding(contentType);
             request.setEntity(entity);
             org.apache.http.HttpResponse response = httpClient.execute(request);
             return createCustomResponseFrom(response);
         } catch (Exception e) {
-        	e.printStackTrace();
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
     public static HttpResponse get(String url, String payload, String username, String password) {
-    	return get(url, payload, AuthType.BASIC, username+":"+password);
+        return get(url, payload, AuthType.BASIC, username + ":" + password);
     }
-    
+
     public static HttpResponse delete(String url, String payload, String username, String password) {
-    	return delete(url, payload, AuthType.BASIC, username+":"+password);
+        return delete(url, payload, AuthType.BASIC, username + ":" + password);
     }
-    
+
     public static HttpResponse get(String url, String payload) {
-    	return get(url, payload, AuthType.NONE, "");
+        return get(url, payload, AuthType.NONE, "");
     }
-    
+
     public static HttpResponse getWithToken(String url, String payload, String token) {
-    	return get(url, payload, AuthType.BASIC, token);
+        return get(url, payload, AuthType.BASIC, token);
     }
-    
+
     public static HttpResponse get(String url, String payload, AuthType authType, String authString) {
         try {
-        	HttpGet request = (HttpGet) makeConnection(url, payload, RequestMethod.GET, authType, authString);
+            HttpGet request = (HttpGet) makeConnection(url, payload, RequestMethod.GET, authType, authString);
             org.apache.http.HttpResponse response = httpClient.execute(request);
             return createCustomResponseFrom(response);
         } catch (Exception e) {
@@ -116,7 +116,7 @@ public class HttpUtil {
 
     public static HttpResponse delete(String url, String payload, AuthType authType, String authString) {
         try {
-        	HttpDelete request = (HttpDelete) makeConnection(url, payload, RequestMethod.DELETE, authType, authString);
+            HttpDelete request = (HttpDelete) makeConnection(url, payload, RequestMethod.DELETE, authType, authString);
             org.apache.http.HttpResponse response = httpClient.execute(request);
             return createCustomResponseFrom(response);
         } catch (Exception e) {
@@ -125,64 +125,61 @@ public class HttpUtil {
     }
 
     static HttpResponse createCustomResponseFrom(org.apache.http.HttpResponse response) throws IOException {
-	    int statusCode = response.getStatusLine().getStatusCode();
-	    String entity = "";
-	    if(response.getEntity() != null) {
-	        entity = IOUtils.toString(response.getEntity().getContent());
+        int statusCode = response.getStatusLine().getStatusCode();
+        String entity = "";
+        if (response.getEntity() != null) {
+            entity = IOUtils.toString(response.getEntity().getContent());
         }
 
         return new HttpResponse(checkSuccessBasedOnHttpCode(statusCode), statusCode, entity);
     }
 
     static boolean checkSuccessBasedOnHttpCode(int httpCode) {
-        if(httpCode>=400 && httpCode<=599) {
-	        return false;
-        }else {
-	        return true;
+        if (httpCode >= 400 && httpCode <= 599) {
+            return false;
+        } else {
+            return true;
         }
     }
 
     static HttpRequestBase makeConnection(String url, String payload, RequestMethod method, AuthType authType, String authString) throws URISyntaxException {
-    	String charset = "UTF-8";
+        String charset = "UTF-8";
 
-        if(url.endsWith("/")){
-        	url = url.substring(0, url.lastIndexOf("/"));
+        if (url.endsWith("/")) {
+            url = url.substring(0, url.lastIndexOf("/"));
         }
-        url = (url+(StringUtils.isBlank(payload)?"":("?"+payload))).replaceAll(" ", "%20");
-    	URI urlo = new URI(url);
+        url = (url + (StringUtils.isBlank(payload) ? "" : ("?" + payload))).replaceAll(" ", "%20");
+        URI urlo = new URI(url);
 
-    	HttpRequestBase requestBase = null;
-    	if(method.equals(RequestMethod.GET)){
-    		requestBase = new HttpGet(urlo);
-    	}
-    	else if(method.equals(RequestMethod.POST)){
-    		requestBase = new HttpPost(urlo);
-    	}
-    	else if(method.equals(RequestMethod.PUT)){
-    		requestBase = new HttpPut(urlo);
-    	}
-    	else if(method.equals(RequestMethod.DELETE)){
-    		requestBase = new HttpDelete(urlo);
-    	}
-    	requestBase.setURI(urlo);
-    	requestBase.addHeader("Accept-Charset", charset);
-		
-		if(authType.name().equalsIgnoreCase("basic")){
-			String encoded = authString.matches(".+:.+")?new String(Base64.encodeBase64(authString.getBytes())):authString;
-			requestBase.addHeader("Authorization", "Basic "+encoded);
-		}
-		else if(authType.name().equalsIgnoreCase("token")){
-			requestBase.addHeader("Authorization", "Token "+authString);
-		}
-		
-		System.out.println(url);
-		return requestBase;
-	}
-    
-    public static String removeEndingSlash(String str){
-		return str.endsWith("/")?str.substring(0, str.lastIndexOf("/")):str;
-	}
-    public static String removeTrailingSlash(String str){
-		return str.startsWith("/")?str.substring(1):str;
-	}
+        HttpRequestBase requestBase = null;
+        if (method.equals(RequestMethod.GET)) {
+            requestBase = new HttpGet(urlo);
+        } else if (method.equals(RequestMethod.POST)) {
+            requestBase = new HttpPost(urlo);
+        } else if (method.equals(RequestMethod.PUT)) {
+            requestBase = new HttpPut(urlo);
+        } else if (method.equals(RequestMethod.DELETE)) {
+            requestBase = new HttpDelete(urlo);
+        }
+        requestBase.setURI(urlo);
+        requestBase.addHeader("Accept-Charset", charset);
+
+        if (authType.name().equalsIgnoreCase("basic")) {
+            String encoded = authString.matches(".+:.+") ? new String(Base64.encodeBase64(authString.getBytes())) : authString;
+            requestBase.addHeader("Authorization", "Basic " + encoded);
+        } else if (authType.name().equalsIgnoreCase("token")) {
+            requestBase.addHeader("Authorization", "Token " + authString);
+        }
+
+        System.out.println(url);
+        return requestBase;
+    }
+
+    public static String removeEndingSlash(String str) {
+        return str.endsWith("/") ? str.substring(0, str.lastIndexOf("/")) : str;
+    }
+
+    public static String removeTrailingSlash(String str) {
+        return str.startsWith("/") ? str.substring(1) : str;
+    }
 }
